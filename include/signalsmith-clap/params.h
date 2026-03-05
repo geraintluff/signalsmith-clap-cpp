c16593b8723d09d586b073166178caf61f4cd91a
Geraint <luffgd@gmail.com>
2026-03-05 10:57:31 +0000

    Add field-member helper, and `ParamManager`

--- include/signalsmith-clap/params.h
+++ include/signalsmith-clap/params.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include "./cpp.h"
+
 #include "clap/events.h"
 #include "clap/ext/params.h"
 
 #include <atomic>
 #include <functional>
+#include <initializer_list>
 
 namespace signalsmith { namespace clap {
 
-/* A parameter object which can send gesture/value events back to the host when needed.
+/** A parameter object which can send gesture/value events back to the host when needed.
 
 It can also track whether its value has been sent to the UI or not, but doesn't specify how that should be done.
 */
@@ -43,6 +46,12 @@ struct Param {
 		sentValue.test_and_set();
 		sentGestureStart.test_and_set();
 		sentGestureEnd.test_and_set();
+	}
+	Param(const Param &other) = delete;
+	
+	void invalidate() {
+		sentUiState.clear();
+		sentValue.clear();
 	}
 	
 	void setValueFromEvent(const clap_event_param_value &paramEvent) {
@@ -97,6 +106,124 @@ struct Param {
 			outEvents->try_push(outEvents, &event.header);
 		}
 	}
+};
+
+/** A collection of Parameters */
+struct ParamManager {
+
+	ParamManager & add() {
+		return *this;
+	}
+	template<class... Others>
+	ParamManager & add(Param &param, Others ...others) {
+		paramList.push_back(&param);
+		return add(others...);
+	}
+	
+	void init(const clap_host *host) {
+		getHostExtension(host, CLAP_EXT_PARAMS, hostParams);
+	}
+	
+	void clearUiState() {
+		for (auto *param : paramList) {
+			param->sentUiState.clear();
+		}
+	}
+
+	void sendEvents(const clap_output_events *eventsOut) {
+		for (auto *param : paramList) {
+			param->sendEvents(eventsOut);
+		}
+	}
+	
+	bool processEvent(const clap_event_header *event) {
+		if (event->space_id != CLAP_CORE_EVENT_SPACE_ID) return false;
+		if (event->type == CLAP_EVENT_PARAM_VALUE) {
+			auto &eventParam = *(const clap_event_param_value *)event;
+			if (eventParam.cookie) {
+				// if provided, it's the parameter
+				auto *param = (Param *)eventParam.cookie;
+				param->setValueFromEvent(eventParam);
+			} else {
+				// Otherwise, match the ID
+				for (auto *param : paramList) {
+					if (eventParam.param_id == param->info.id) {
+						param->setValueFromEvent(eventParam);
+						break;
+					}
+				}
+			}
+			return true;
+
+		}
+		return false;
+	}
+	
+	template<auto memberPtr>
+	const void * ext() {
+		static const clap_plugin_params ext{
+			.count=pluginMemberMethod<memberPtr, &ParamManager::paramsCount>(),
+			.get_info=pluginMemberMethod<memberPtr, &ParamManager::paramsGetInfo>(),
+			.get_value=pluginMemberMethod<memberPtr, &ParamManager::paramsGetValue>(),
+			.value_to_text=pluginMemberMethod<memberPtr, &ParamManager::paramsValueToText>(),
+			.text_to_value=pluginMemberMethod<memberPtr, &ParamManager::paramsTextToValue>(),
+			.flush=pluginMemberMethod<memberPtr, &ParamManager::paramsFlush>(),
+		};
+		return &ext;
+	}
+
+	uint32_t paramsCount() {
+		return uint32_t(paramList.size());
+	}
+
+	bool paramsGetInfo(uint32_t index, clap_param_info *info) {
+		if (index >= paramList.size()) return false;
+		*info = paramList[index]->info;
+		return true;
+	}
+	
+	bool paramsGetValue(clap_id paramId, double *value) {
+		for (auto *param : paramList) {
+			if (param->info.id == paramId) {
+				*value = param->value;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	bool paramsValueToText(clap_id paramId, double value, char *text, uint32_t textCapacity) {
+		for (auto *param : paramList) {
+			if (param->info.id == paramId) {
+				if (param->formatFn) {
+					auto str = param->formatFn(value);
+					std::strncpy(text, str.c_str(), textCapacity);
+				} else {
+					std::snprintf(text, textCapacity, param->formatString, value);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool paramsTextToValue(clap_id paramId, const char *text, double *value) {
+		return false; // not supported (yet)
+	}
+	
+	void paramsFlush(const clap_input_events *eventsIn, const clap_output_events *eventsOut) {
+		uint32_t eventCount = eventsIn->size(eventsIn);
+		for (uint32_t i = 0; i < eventCount; ++i) {
+			auto *event = eventsIn->get(eventsIn, i);
+			processEvent(event);
+			eventsOut->try_push(eventsOut, event);
+		}
+		sendEvents(eventsOut);
+	}
+
+private:
+	std::vector<Param *> paramList;
+	const clap_host_params *hostParams = nullptr;
 };
 
 }} // namespace
